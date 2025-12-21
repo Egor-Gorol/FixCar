59f79012b013511cda7e22ff62ef2486a9f49c37
Egor Gorol <antonuk030403@gmail.com>
2025-12-21 21:36:09 +0200

    front

--- routes/frontend.py
+++ routes/frontend.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
-#import httpx
+import httpx
 from routes.auth import get_current_user
 
 templates = Jinja2Templates(directory="templates")
@@ -12,25 +12,26 @@ async def home(request: Request, error: str | None = None):
     return templates.TemplateResponse("index.html", {"request": request, "error": error})
 
 
-# @router.get("/account")
-# async def account_dashboard(request: Request, current_user: dict = Depends(get_current_user)):
-#     async with httpx.AsyncClient() as client:
-#         response = await client.get(
-#             f"http://localhost:8000/account/user/me",
-#             headers={"Authorization": f"Bearer {request.cookies.get('access_token')}"}
-#         )
-#         user_data = response.json()
+@router.get("/account")
+async def account_dashboard(request: Request, current_user: dict = Depends(get_current_user)):
+    async with httpx.AsyncClient() as client:
+         response = await client.get(
+            f"http://localhost:8000/account/user/me",
+            headers={"Authorization": f"Bearer {request.cookies.get('access_token')}"}
+         )
+         user_data = response.json()
 
-#     return templates.TemplateResponse(
-#         "account/dashboard.html", {"request": request, "user_data": user_data}
-#     )
-
+    return templates.TemplateResponse("account_dashboard.html", {"request": request, "user_data": user_data})
 
 @router.get("/auth/register")
-async def home(request: Request, error: str | None = None):
+async def register(request: Request, error: str | None = None):
     return templates.TemplateResponse("registr.html", {"request": request, "error": error})
 
 
 @router.get("/auth/login")
-async def home(request: Request, error: str | None = None):
+async def login(request: Request, error: str | None = None):
     return templates.TemplateResponse("login.html", {"request": request, "error": error})
+
+@router.get("/requests/new")
+async def new_request(request: Request, error: str | None = None):
+    return templates.TemplateResponse("request_new.html", {"request": request, "error": error})
